4de42ceb351a39f96d21f601f5fb24d682b907a2
RomSribn <steeper.fest@gmail.com>
2018-12-08 11:19:21 +0200

    True response
    Failed connection to server ( this one don't exist)
    trying 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'

--- script.js
+++ script.js
@@ -3,75 +3,166 @@ const log = txt => console.log(txt);
 
 
 
-var x, i, j, selElmnt, a, b, c;
-/*look for any elements with the class "custom-select":*/
-x = document.getElementsByClassName("custom-select");
-for (i = 0; i < x.length; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  /*for each element, create a new DIV that will act as the selected item:*/
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  /*for each element, create a new DIV that will contain the option list:*/
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < selElmnt.length; j++) {
-    /*for each option in the original select element,
-    create a new DIV that will act as an option item:*/
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function(e) {
-        /*when an item is clicked, update the original select box,
-        and the selected item:*/
-        var y, i, k, s, h;
-        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-        h = this.parentNode.previousSibling;
-        for (i = 0; i < s.length; i++) {
-          if (s.options[i].innerHTML == this.innerHTML) {
-            s.selectedIndex = i;
-            h.innerHTML = this.innerHTML;
-            y = this.parentNode.getElementsByClassName("same-as-selected");
-            for (k = 0; k < y.length; k++) {
-              y[k].removeAttribute("class");
-            }
-            this.setAttribute("class", "same-as-selected");
-            break;
-          }
-        }
-        h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function(e) {
-      /*when the select box is clicked, close any other select boxes,
-      and open/close the current select box:*/
-      e.stopPropagation();
-      closeAllSelect(this);
-      this.nextSibling.classList.toggle("select-hide");
-      this.classList.toggle("select-arrow-active");
-  });
+const regBtn = {
+  signUpBtn: document.querySelector('#SignIn'),
+  signUpForm: document.querySelector('.signup'),
+  signInBtn: document.querySelector('#SignUp'),
+  signInForm: document.querySelector('.login'),
+  signBtn: document.querySelectorAll('#SignBtn'),
+  sendBtn: document.querySelectorAll('.register-button')
+  
 }
-function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
-  var x, y, i, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  for (i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
-    } else {
-      y[i].classList.remove("select-arrow-active");
+const mainForm = document.querySelector('.register')
+// log(mainForm)
+
+let dataFromForm = new FormData(mainForm);
+
+// var details = {
+//     'userName': 'test@gmail.com',
+//     'password': 'Password!',
+//     'grant_type': 'password'
+// };
+
+const frForm = {
+  "agres": "on", 
+    "email": "123432456@sdf.com", 
+    "gender": "male", 
+    "name": "sadfsadfsadf", 
+    "pass": "123", 
+    "secondname": "asfasdfsdf"
     }
-  }
-  for (i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
+
+let formBody = [];
+function getEncode(data){
+
+for (let property in data) {
+  const encodedKey = encodeURIComponent(property);
+  // log(encodedKey)
+  const encodedValue = encodeURIComponent(data[property]);
+  // log(encodedValue)
+  formBody.push(encodedKey + "=" + encodedValue);
 }
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
-document.addEventListener("click", closeAllSelect);
+formBody = formBody.join("&");
+// return formBody
+}
+
+// log(getEncode({"agres":"on","email":"123432456@sdf.com","gender":"male","name":"sfsfsddfsdf","pass":"sfsfsdd23fsdf","secondname":"sfsfsddfsdf"}))
+
+
+// log(formBody)
+
+
+// const params = {
+//   method: 'post',
+//   body: {
+//   "agres": "on", 
+//     "email": "123432456@sdf.com", 
+//     "gender": "male", 
+//     "name": "sadfsadfsadf", 
+//     "pass": "123", 
+//     "secondname": "asfasdfsdf"
+//     },
+//   headers: {
+//     'Content-Type':'application/json'
+//   }
+// }
+
+
+
+// console.log(regBtn.signUpForm);
+// console.log(regBtn.signUpForm.classList.contains('show'));
+// regBtn.sendBtn.forEach(btn => btn.addEventListener('click', handleSubmit));
+regBtn.signBtn.forEach(btn => btn.addEventListener('click', handleToggle));
+// regBtn.sendBtn.forEach(btn => btn.addEventListener('submit', handleSubmit));
+mainForm.addEventListener('submit', handleSubmit)
+// regBtn.signUpBtn.addEventListener('click', handleShowSignUp);
+// regBtn.signInBtn.addEventListener('click', handleShowSignIn);
+
+
+// function handleShowSignUp(){
+//   if(regBtn.signUpForm.contains('show')){
+//     regBtn.signUpForm.remove('show');
+//   }
+// }
+
+
+// function handleShowSignIn(){
+//   if(regBtn.signInForm.contains('show')) return
+// }
+
+function JSONtoObj(json){
+  return JSON.parse(json).form
+}
+
+
+function handleSubmit(evt){
+ 
+  event.preventDefault();
+
+    var request = new XMLHttpRequest();
+    // POST to httpbin which returns the POST data as JSON
+    request.open('POST', 'https://httpbin.org/post', /* async = */ false);
+
+    var formData = new FormData(mainForm);
+
+    // formData.append('appended1', 'appended value');
+
+    request.send(formData);
+
+    //console.log(JSON.parse(request.response).form);
+    // console.log(request.response);
+
+    getValid(JSONtoObj(request.response))
+    .then(response => log(response.json));
+    // .then(data => log(data.json()));
+}
+
+
+function handleToggle(event){
+  // event.preventDefault()
+  let evt = event.target;
+  console.log(evt.id);
+
+  if(evt.id === 'SignIn' && regBtn.signInForm.classList.contains('show')){
+    regBtn.signInForm.classList.remove('show');
+    regBtn.signUpForm.classList.add('show');
+  }
+  if(evt.id === 'SignUp' && regBtn.signUpForm.classList.contains('show')){
+    log('up')
+    regBtn.signUpForm.classList.remove('show');
+    regBtn.signInForm.classList.add('show');
+  }
+
+}
+
+
+
+function getValid(data){
+// log(data)
+getEncode(JSON.stringify(data))
+log(formBody)
+  return fetch('http://codeit.pro/codeitCandidates/serverFrontendTest/user/registration', {
+    method: 'POST',
+  body: formBody,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+  },
+  })
+}
+
+// getValid().then(data => log(data))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
